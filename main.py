6d5a6b1eab6ed7dabf74f6da7a12a3984c837aed
uchansansan <uchansansan@gmail.com>
2021-10-11 13:44:18 +0700

    add primetive gui

---
+++ main.py
@@ -0,0 +1,35 @@
+import design2 as design
+import sys
+from PyQt5 import QtWidgets
+
+
+class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.sequence = 0
+        self.site = 0
+
+    def SaveSeq(self):
+        if self.pushButton.isChecked():
+            #self.label_2.setText(self.lineEdit.text())
+            self.sequence = self.lineEdit.text()
+            self.textBrowser.append(self.sequence)
+
+    def SaveSite(self):
+        if self.pushButton_2.isChecked():
+            #self.label.setText(self.lineEdit_2.text())
+            self.site = self.lineEdit_2.text()
+            self.textBrowser.append(self.site)
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = ExampleApp()
+    window.show()
+    #window.SaveSequance()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
